a8451f92927ff28bd34b11bf88332d3b2f72542a
Mateusz Wu <15673167+worotyns@users.noreply.github.com>
2025-05-21 14:12:16 +0200

    fix(readme): fix sw.js content

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,13 @@
 // This script is located on 'self' origin
 
+// Listen for messages from the main thread
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'PING') {
+    console.log('Service Worker received ping');
+    // Respond to the ping with a pong
+    event.ports[0].postMessage({ type: 'PONG' });
+  }
+});
+
 // Import the worker script from third party resource
-// importScripts('http://localhost:8888/worker.js');
-
-fetch('http://localhost:8888/worker.js', {
-  integrity: 'sha384-T068YnjR6cJn0WcYOTjC6rfSsc2GOXcbPl6CL0DVbDDZhkRcShn/SapO9C8q1/8Z'
-}).then(() => {
-
-  // Listen for messages from the main thread
-  self.addEventListener('message', (event) => {
-    if (event.data && event.data.type === 'PING') {
-      console.log('Service Worker received ping');
-      // Respond to the ping with a pong
-      event.ports[0].postMessage({ type: 'PONG' });
-    }
-  });
-
-  console.log('Worker script loaded via importScripts');
-  importScripts('http://localhost:8888/worker.js');
-})
+importScripts('http://localhost:8888/worker.js');
